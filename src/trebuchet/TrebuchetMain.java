eed87d57c697da1e0e15620c17df973a85f20c15
Rashad Kadado <rkadado@gmail.com>
2015-11-16 09:54:02 -0600

    renamed trebuchet to trebuchetmain

--- src/trebuchet/TrebuchetMain.java
+++ src/trebuchet/TrebuchetMain.java
@@ -9,13 +9,13 @@ package trebuchet;
  *
  * @author s355569
  */
-public class Trebuchet {
+public class TrebuchetMain {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
