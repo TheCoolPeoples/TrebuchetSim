dbd1d59294714a08397ec3eb25b440771860e367
Rashad Kadado <rkadado@gmail.com>
2015-11-20 10:39:40 -0600

    fixed derivative stuff and finding derivatives

--- src/trebuchet/RungeKutta.java
+++ src/trebuchet/RungeKutta.java
@@ -15,8 +15,11 @@ public class RungeKutta {
     
     Function stage1 = new Function() {
         
+        private double X1=0, X2=0, X3=0, X4=0, X5=0, X6=0;
+        private double x1,x2,x3;
+        private double y1,y2,y3;
         private double m1,m2,m3;
-        private double l1 ,l2,l3,l4, s;
+        private double l1 ,l2,l3,l4, s, h;
         private double I; 
         private double g = 9.81;
         private double angle1, angle2;
@@ -30,7 +33,49 @@ public class RungeKutta {
             return m2*l1*l4*Math.sin(angle2-angle1);
         }
         
+        private double findK3() {
+            return (m3*l2*(x3*Math.sin(angle1) + h*Math.cos(angle1)))/(x3+l2*Math.cos(angle1));
+        }
         
+        private double findK4() {
+            return -m2*l1*l4*X4*X4+Math.cos(angle2-angle1) - g*(m2*l1-m1*(l2-s))*Math.cos(angle1);
+        }
+        
+        private double findK5() {
+            return l1*Math.sin(angle2-angle1);
+        }
+        
+        private double findK6() {
+            return l4;
+        }
+        
+        private double findK7() {
+            return l1*X2*X2*Math.cos(angle2-angle1) - g*Math.sin(angle2);
+        }
+        
+        private double findK8() {
+            return -h*l2*Math.cos(angle1)-l2*x3*Math.sin(angle1);
+        }
+        
+        private double findK9() {
+            return l2*Math.cos(angle1) + x3;
+        }
+        
+        private double findK10() {
+            return -X6*X6*x3+2*l2*X2*X6*Math.sin(angle1)+l2*X2*X2*(x3*Math.cos(angle1)-h*Math.sin(angle1));
+        }
+        
+        private double find2DevAngle1() {
+            return (k10*k3*k6-k4*k6*k9+k7*k9)/(-k1*k6*k9+k2*k5*k9+k3*k6*k8);
+        }
+        
+        private double find2DevAngle2() {
+            return (k3*(k7*k8-k10*k5)+k4*k5*k9-k1*k7*k9)/(-k1*k6*k9+k2*k5*k9+k3*k6*k8);
+        }
+        
+        private double find2Devx3() {
+            return (-k1*k10*k6+k4*k6*k8+k2*(k10*k5-k7*k8))/(-k1*k6*k9+k2*k5*k9+k3*k6*k8);
+        }
         
         @Override
         public double apply(double t, double y) {
